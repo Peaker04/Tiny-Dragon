2ed48600c9cf32424d60d80167721e9c9801bfe2
Datlee <lvd27012004@gmail.com>
2026-06-02 20:20:57 +0700

    feat: setup player character controller, animations, physics, and sprites

---
+++ Assets/_Project/Scripts/Player/PlayerController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
+
+    private Rigidbody2D rb;
+    private float moveX;
+    private bool jumpPressed;
+    private bool isGrounded;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        moveX = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpPressed = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(
+            groundCheck.position,
+            0.2f,
+            groundLayer
+        );
+
+        rb.linearVelocity = new Vector2(moveX * moveSpeed, rb.linearVelocity.y);
+
+        if (jumpPressed && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+        jumpPressed = false;
+    }
+}
