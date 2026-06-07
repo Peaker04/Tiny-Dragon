7986ff03dc68deb93e0850e9f9303627e6e0acc6
conghieu <hieuvcde181064@fpt.edu.vn>
2026-06-07 22:19:28 +0700

    Add Editor settings folder

---
+++ Assets/_Project/Editor/StartFromSpecificScene.cs
@@ -0,0 +1,30 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class StartFromSpecificScene
+{
+    static StartFromSpecificScene()
+    {
+        // Tên scene bạn muốn luôn bắt đầu
+        string sceneName = "Level_01_Original";
+
+        // Tìm scene trong project
+        string[] guids = AssetDatabase.FindAssets("t:Scene " + sceneName);
+
+        if (guids.Length > 0)
+        {
+            // Lấy đường dẫn của scene đầu tiên tìm thấy
+            string path = AssetDatabase.GUIDToAssetPath(guids[0]);
+            SceneAsset sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(path);
+
+            // Ép Unity luôn chạy scene này khi bấm Play
+            EditorSceneManager.playModeStartScene = sceneAsset;
+        }
+        else
+        {
+            Debug.LogWarning("Không tìm thấy scene tên là: " + sceneName);
+        }
+    }
+}
