968a7185323058b671829ca31b35c6ae0cc65c7d
Tai <taithptqt@gmail.com>
2026-06-22 12:03:38 +0700

    change all map

---
+++ Assets/_Project/Scripts/Player/SceneExitOnPlayerContact.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider2D))]
+public sealed class SceneExitOnPlayerContact : MonoBehaviour
+{
+    [SerializeField] private string targetSceneName;
+    [SerializeField] private bool useTargetSpawnPosition;
+    [SerializeField] private Vector3 targetSpawnPosition;
+    [SerializeField] private float targetFacingDirection = 1f;
+
+    private bool isLoadingScene;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        TryLoadTargetScene(collision.collider);
+        TryLoadTargetScene(collision.otherCollider);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        TryLoadTargetScene(other);
+    }
+
+    private void TryLoadTargetScene(Collider2D playerCollider)
+    {
+        if (isLoadingScene || string.IsNullOrWhiteSpace(targetSceneName))
+        {
+            return;
+        }
+
+        if (playerCollider.GetComponentInParent<PlayerController>() == null)
+        {
+            return;
+        }
+
+        isLoadingScene = true;
+        if (useTargetSpawnPosition)
+        {
+            PlayerSceneTransition.LoadSceneWithPlayerSpawn(
+                targetSceneName,
+                targetSpawnPosition,
+                targetFacingDirection
+            );
+            return;
+        }
+
+        SceneManager.LoadScene(targetSceneName, LoadSceneMode.Single);
+    }
+}
